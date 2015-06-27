bb1084260bb6c440db099db7772cbbb44e312940
Daniel Klemm <daniel.klemm@datameer.com>
2015-06-27 18:05:52 +0200

    Added PluginOperatorTest

--- app/src/androidTest/java/gamercompanion/test/PluginOperatorTest.java
+++ app/src/androidTest/java/gamercompanion/test/PluginOperatorTest.java
@@ -2,9 +2,14 @@ package gamercompanion.test;
 
 import android.test.AndroidTestCase;
 
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableCollection;
+
 import junit.framework.TestCase;
 
 import gamercompanion.src.activities.controlling.ActivityController;
+import gamercompanion.src.dataObjects.plugin.Plugin;
+import gamercompanion.src.dataOperator.PluginOperator;
 
 /**
  * Created by dklemm on 27.06.15.
@@ -18,10 +23,12 @@ public class PluginOperatorTest extends AndroidTestCase {
     }
 
     public void testActivatedPlugins() throws Exception {
-
+        ImmutableCollection<Plugin> plugins = PluginOperator.activatedPlugins();
+        assertTrue(plugins.size()>0);
     }
 
     public void testActivatedPluginNames() throws Exception {
-
+        FluentIterable<String> pluginNames = PluginOperator.activatedPluginNames();
+        assertTrue(pluginNames.contains("News") || pluginNames.contains("GameDB"));
     }
 }

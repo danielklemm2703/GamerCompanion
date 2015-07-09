f87e10e7f70c56330288976e814aa45193efa2ca
Daniel Klemm <daniel.klemm@datameer.com>
2015-07-09 22:24:46 +0200

    Implemented Skeleton of GameDBMenu and LaunchCalendarMenu

--- app/src/main/java/gamercompanion/src/activities/GameDBMenu.java
+++ app/src/main/java/gamercompanion/src/activities/GameDBMenu.java
@@ -3,6 +3,11 @@ package gamercompanion.src.activities;
 import android.os.Bundle;
 
 import gamercompanion.src.activities.controlling.ControlledActivity;
+import static gamercompanion.src.userInterface.GameDBMenuUserInterface.*;
+import gamercompanion.src.utils.Unit;
+import gamercompanion.src.utils.tryUtil.Try;
+
+import static gamercompanion.src.error.ErrorUtil.showWarning;
 
 /*
 * This class implements overview of the GameDB, displaying the trcaked platforms
@@ -11,5 +16,11 @@ public class GameDBMenu extends ControlledActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Try<Unit> tryLayout = drawGameDBMenuInterface();
+        if(!tryLayout.isSuccess())
+        {
+            showWarning(tryLayout.failure().getMessage());
+        }
     }
 }
--- app/src/main/java/gamercompanion/src/activities/LaunchCalendarMenu.java
+++ app/src/main/java/gamercompanion/src/activities/LaunchCalendarMenu.java
@@ -3,6 +3,11 @@ package gamercompanion.src.activities;
 import android.os.Bundle;
 
 import gamercompanion.src.activities.controlling.ControlledActivity;
+import static gamercompanion.src.userInterface.LaunchCalendarMenuUserInterface.*;
+import gamercompanion.src.utils.Unit;
+import gamercompanion.src.utils.tryUtil.Try;
+
+import static gamercompanion.src.error.ErrorUtil.*;
 
 /**
  * This class implements overview of the LaunchCalender, displaying the trcaked platforms
@@ -11,5 +16,11 @@ public class LaunchCalendarMenu extends ControlledActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Try<Unit> tryLayout = drawLaunchCalendarMenuInterface();
+        if(!tryLayout.isSuccess())
+        {
+            showWarning(tryLayout.failure().getMessage());
+        }
     }
 }
